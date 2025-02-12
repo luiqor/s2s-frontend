df6783d2100c25f813d2ec497e7afc1e5041e2a8
luiqor <vohnenna@gmail.com>
2025-02-12 14:01:44 +0200

    replace LessonsContainer props

--- src/containers/my-resources/lessons-container/LessonsContainer.tsx
+++ src/containers/my-resources/lessons-container/LessonsContainer.tsx
@@ -26,6 +26,7 @@ import { adjustColumns, getScreenBasedLimit } from '~/utils/helper-functions'
 import { useModalContext } from '~/context/modal-context'
 import ChangeResourceConfirmModal from '~/containers/change-resource-confirm-modal/ChangeResourceConfirmModal'
 import { getFullUrl } from '~/utils/get-full-url'
+import useMutation from '~/hooks/use-mutation'
 
 const LessonsContainer = () => {
   const navigate = useNavigate()
@@ -35,8 +36,8 @@ const LessonsContainer = () => {
   const searchTitle = useRef<string>('')
   const breakpoints = useBreakpoints()
   const [selectedItems, setSelectedItems] = useState<string[]>([])
-  const { handleErrorAlert } = useSnackbarAlert()
   const queryClient = useQueryClient()
+  const { handleErrorAlert, handleSuccessAlert } = useSnackbarAlert()
 
   const { sort } = sortOptions
   const itemsPerPage = getScreenBasedLimit(breakpoints, itemsLoadLimit)
@@ -56,10 +57,14 @@ const LessonsContainer = () => {
     })
   }, [page, itemsPerPage, sort, searchTitle, selectedItems])
 
-  const deleteLesson = useCallback(
-    (id?: string) => ResourceService.deleteLesson(id ?? ''),
-    []
-  )
+  const { mutate: handleDeleteLesson } = useMutation({
+    mutationFn: ResourceService.deleteLessonQuery,
+    onError: handleErrorAlert,
+    onSuccess: () => {
+      handleSuccessAlert(`myResourcesPage.lessons.successDeletion`)
+    },
+    queryKey: ['lessons']
+  })
 
   const {
     data: lessons,
@@ -125,10 +130,10 @@ const LessonsContainer = () => {
       response: lessons ?? defaultResponses.itemsWithCount,
       getData: handleInvalidateLessons
     },
-    services: { deleteService: deleteLesson },
+    services: { deleteService: handleDeleteLesson },
     itemsPerPage,
-    actions: { onEdit },
-    resource: ResourcesTabsEnum.Lessons,
+    actions: { onEdit, onDelete: handleDeleteLesson },
+    resourceType: ResourcesTabsEnum.Lessons,
     sort: sortOptions,
     pagination: { page, onChange: handleChangePage }
   }
